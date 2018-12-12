502e5f0dbedc4094391d5f92e4ddf439fadaabae
Justin Simerly <justinsimerly1992@gmail.com>
2018-12-12 13:19:24 -0500

    Restructure to use Operation and Notice

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,20 @@ pub struct Paper {
     first_line: usize,
 }
 
+enum Operation {
+    Noop,
+    End,
+    ChangeToCommand,
+    ScrollDown,
+    ScrollUp,
+    SeeView(String),
+    EditCommand(char),
+}
+
+enum Notice {
+    Quit,
+}
+
 impl Paper {
     pub fn new() -> Paper {
         let window = pancurses::initscr();
@@ -41,93 +55,107 @@ impl Paper {
 
     pub fn run(&mut self) {
         loop {
-            if let Err(()) = self.process_input() {
-                break;
+            let operation = self.process_input();
+
+            match self.operate(operation) {
+                Some(Notice::Quit) => break,
+                None => (),
             }
         }
 
         pancurses::endwin();
     }
 
-    fn process_input(&mut self) -> Result<(), ()> {
-        match self.window.getch() {
-            Some(Input::Character(c)) => return self.process_char(c),
-            _ => (),
-        }
-        
-        Ok(())
-    }
-
-    fn process_char(&mut self, c: char) -> Result<(), ()> {
-        match self.mode {
-            Mode::Display => {
-                match c {
-                    '.' => {
-                        self.window.mv(0, 0);
-                        self.mode = Mode::Command;
-                        self.command.clear();
-                    },
-                    'j' => {
-                        self.first_line = cmp::min(self.first_line + self.scroll_height(), self.view.lines().count() - 1);
-                        self.write_view();
-                    },
-                    'k' => {
-                        let movement = self.scroll_height();
-
-                        if self.first_line < movement {
-                            self.first_line = 0;
-                        } else {
-                            self.first_line -= movement;
-                        }
-                        self.write_view();
-                    },
-                    _ => (),
-                }
+    fn operate(&mut self, op: Operation) -> Option<Notice> {
+        match op {
+            Operation::ChangeToCommand => {
+                self.window.mv(0, 0);
+                self.command.clear();
+                self.mode = Mode::Command;
             },
-            Mode::Command => {
-                if c == '\n' {
-                    let re = Regex::new(r"(?P<command>.+?)(?:\s|$)").unwrap();
-                    let cmd = self.command.clone();
+            Operation::ScrollDown => {
+                self.first_line = cmp::min(self.first_line + self.scroll_height(), self.view.lines().count() - 1);
+                self.write_view();
+            },
+            Operation::ScrollUp => {
+                let movement = self.scroll_height();
 
-                    match re.captures(&cmd) {
-                        Some(caps) => return self.process_command(&caps["command"]),
-                        None => (),
-                    }
+                if self.first_line < movement {
+                    self.first_line = 0;
                 } else {
-                    self.window.addch(c);
+                    self.first_line -= movement;
+                }
+                self.write_view();
+            },
+            Operation::EditCommand(c) => {
+                self.window.addch(c);
 
-                    if c == '\u{08}' {
-                        self.command.pop();
-                        // Backspace moves cursor back one but does not delete the character.
-                        self.window.delch();
-                    } else {
-                        self.command.push(c);
-                    }
+                if c == '\u{08}' {
+                    self.command.pop();
+                    // Backspace moves cursor back one but does not delete the character.
+                    self.window.delch();
+                } else {
+                    self.command.push(c);
                 }
             },
-        }
-
-        Ok(())
-    }
-
-    fn process_command(&mut self, command: &str) -> Result<(), ()> {
-        match command {
-            "see" => {
-                let re = Regex::new(r"see\s*(?P<file>.*)").unwrap();
-                {
-                    let filename = &re.captures(&self.command).unwrap()["file"];
-
-                    self.mode = Mode::Display;
-                    self.view = fs::read_to_string(&filename).unwrap();
-                }
+            Operation::SeeView(path) => {
+                self.mode = Mode::Display;
+                self.view = fs::read_to_string(&path).unwrap();
                 self.first_line = 0;
                 self.write_view();
             },
-            "end" => return Err(()),
-            _ => (),
+            Operation::End => {
+                return Some(Notice::Quit)
+            },
+            Operation::Noop => (),
         }
 
-        Ok(())
+        None
+    }
+
+    fn process_input(&self) -> Operation {
+        match self.window.getch() {
+            Some(Input::Character(c)) => self.process_char(c),
+            _ => Operation::Noop,
+        }
+    }
+
+    fn process_char(&self, c: char) -> Operation {
+        match self.mode {
+            Mode::Display => {
+                match c {
+                    '.' => Operation::ChangeToCommand,
+                    'j' => Operation::ScrollDown,
+                    'k' => Operation::ScrollUp,
+                    _ => Operation::Noop,
+                }
+            },
+            Mode::Command => {
+                match c {
+                    '\n' => {
+                        let re = Regex::new(r"(?P<command>.+?)(?:\s|$)").unwrap();
+                        let cmd = self.command.clone();
+
+                        match re.captures(&cmd) {
+                            Some(caps) => return self.process_command(&caps["command"]),
+                            None => Operation::Noop,
+                        }
+                    },
+                    _ => Operation::EditCommand(c),
+                }
+            },
+        }
+    }
+
+    fn process_command(&self, command: &str) -> Operation {
+        match command {
+            "see" => {
+                let re = Regex::new(r"see\s*(?P<path>.*)").unwrap();
+                Operation::SeeView(re.captures(&self.command).unwrap()["path"].to_string())
+            },
+            "end" => Operation::End,
+            _ => Operation::Noop,
+        }
     }
 
     fn write_view(&mut self) {
