ec440b7fb32e4cc809be1bf799b5ef72380341ee
Justin Simerly <justinsimerly1992@gmail.com>
2020-01-10 16:48:35 -0500

    Add wrap configuration (#91)
    * Initial wrap functionality
    
    * Add watching config file
    
    * Flatten ui::Input enum
    
    * Add glitches

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,14 @@
-//! A terminal-based text viewer/editor with goals to maximize simplicity and efficiency.
-//!
-//! ## Organization
-//! 1) Name
-//! GIVEN:
-//!     - The goal of the application is to provide a way to view and edit text that is as simple as using a pencil and paper.
-//! THEREFORE
-//!     - This application shall be known as `paper`.
+//! A terminal-based text editor with goals to maximize simplicity and efficiency.
 //!
 //! ## Functional Overview
-//! 1) Primary Keys
-//! GIVEN:
-//!     - It is desirable to perform viewing and editing actions as fast as possible.
-//!     - The best speed performance is generally achieved when the user's hands avoid unnecessary movements.
-//!     - The keyboard is the best tool for a user to send a variety of easily understood inputs.
-//!     - The **primary keys** are the keys that are generally accesible from the home row on a majority of keyboards, defined as by the 4 following corners: `\``, `Backspace`, `Left Ctrl`, `Right Ctrl`, and with 1 addtional exception of the `Esc` key.
-//! THEREFORE:
-//!     - All functionality shall able to be performed via the primary keys.
-//!     - Functionality for the mouse and other keys may be added as long as the functionality is also accessible via the primary keys.
+//! 1) All functionality shall be able to be performed via the keys reachable from the home row. Where it makes sense, functionality may additionally be performed via the mouse and other keys.
+//! 2) All input shall be modal, i.e. keys shall implement different functionality depending on the current mode of the application.
+//! 3) Paper shall support the Language Server Protocol.
 //!
-//! 2) Modal Operation
-//! GIVEN:
-//!     - All functionality shall be implemented by the keyboard.
-//!     - Most keys already have an accepted function for inputing text.
-//!     - Adding inputs for more complex operations can be done via either modifiers or modes.
-//! THEREFORE:
-//!     - All input shall be modal, i.e. keys shall implement different operations depending on the current mode of the application.
-//!
-//! 3) Filtering
-//! THEREFORE:
-//!     - Text manipulation shall involve a 3-step process of identifying the location an edit should occur, marking that the location, and then performing the desired edit.
-//!
-//! 4) Language Server Support
-//! GIVEN:
-//!     - The `Language Server Protocol` standardizes the interface between a tool and a language.
-//! THEREFORE:
-//!     - The application shall support the Language Server Protocol for all reasonably acceptable languages.
-//!
-//! ## Implementation
-//! 1) Reuse existing tools
-//! THEREFORE:
-//!     - Paper shall reuse already implemented tools wherever possible.
-//!
-//! 2) Code Quality
-//! THEREFORE:
-//!     - Paper shall follow all cargo-format conventions. Paper shall follow as many clippy lints as reasonably possible.
+//! - Text manipulation shall involve a 3-step process of identifying the location should occur, marking that location, and then performing the desired edit.
+//! - Paper shall reuse already implemented tools wherever possible.
+//! - Paper shall follow all cargo-format conventions.
+//! - Paper shall follow as many clippy lints as reasonably possible.
 #![warn(
     absolute_paths_not_starting_with_crate,
     anonymous_parameters,
@@ -81,9 +46,9 @@
 #![allow(
     clippy::fallible_impl_from, // Not always valid; issues should be detected by tests or other lints.
     clippy::implicit_return, // Goes against rust convention and requires return calls in places it is not helpful (e.g. closures).
+    clippy::large_enum_variant, // Seems to be the same as variant_size_differences.
     clippy::suspicious_arithmetic_impl, // Not always valid; issues should be detected by tests or other lints.
     clippy::suspicious_op_assign_impl, // Not always valid; issues should be detected by tests or other lints.
-    clippy::large_enum_variant, // Generally okay.
     variant_size_differences, // Generally okay.
 )]
 // Temporary allows.
@@ -91,46 +56,53 @@
     clippy::missing_inline_in_public_items, // Flags methods in derived traits.
     clippy::multiple_crate_versions, // Requires redox_users update to avoid multiple versions of rand_core.
     // See <https://gitlab.redox-os.org/redox-os/users/merge_requests/30>
+    clippy::unreachable, // Added by derive(Enum).
 )]
 
 mod app;
 mod translate;
 mod ui;
 
-pub use ui::Settings;
+pub use ui::Arguments;
 
 use {
-    app::{LspError, Mode, Operation, Sheet},
+    app::{LspError, Operation, Sheet},
     log::SetLoggerError,
     simplelog::{Config, LevelFilter, WriteLogger},
-    std::{collections::HashMap, fs::File, io},
+    std::{fs::File, io},
     thiserror::Error,
-    translate::{ConfirmInterpreter, Interpreter, ViewInterpreter},
-    ui::{Change, Input, Terminal},
+    translate::Interpreter,
+    ui::Terminal,
 };
 
 /// Initializes the application logger.
-fn init_logger() -> Result<(), LogError> {
+///
+/// The logger writes all logs to the file `paper.log`.
+fn init_logger() -> Result<(), Failure> {
     let log_filename = "paper.log".to_string();
 
     WriteLogger::init(
         LevelFilter::Trace,
         Config::default(),
-        File::create(&log_filename).map_err(|e| LogError::CreateLogFile(log_filename, e))?,
+        File::create(&log_filename).map_err(|e| Failure::CreateLogFile(log_filename, e))?,
     )?;
     Ok(())
 }
 
-/// Describes the paper application.
+/// Manages the execution of the application.
+///
+/// The runtime loop of the application is as follows:
+/// 1. Receive input.
+/// 2. Translate input into operations.
+/// 3. Execute operations and determine the appropriate changes.
+/// 4. Output the changes.
 #[derive(Debug, Default)]
 pub struct Paper {
-    /// Current [`Mode`] of the application.
-    mode: Mode,
-    /// [`Interpreter`]s supported by the application.
-    interpreters: InterpreterMap,
+    /// Translates input into operations.
+    interpreter: Interpreter,
     /// Interface between the application and the user.
     ui: Terminal,
-    /// The [`Sheet`] of the application.
+    /// Processes application operations.
     sheet: Sheet,
 }
 
@@ -143,9 +115,9 @@ impl Paper {
 
     /// Configures the application and then starts its runtime loop.
     #[inline]
-    pub fn run(&mut self, settings: Settings) -> Result<(), Failure> {
+    pub fn run(&mut self, arguments: Arguments) -> Result<(), Failure> {
         init_logger()?;
-        self.ui.init(settings)?;
+        self.ui.init(arguments)?;
 
         loop {
             if !self.step()? {
@@ -156,21 +128,16 @@ impl Paper {
         Ok(())
     }
 
-    /// Processes a single event from the user, returning if the application should keep running.
+    /// Processes a single input, returning if the application should keep running.
     #[inline]
     fn step(&mut self) -> Result<bool, Failure> {
         let mut keep_running = true;
 
         if let Some(input) = self.ui.input()? {
-            for operation in self.translate(input)? {
-                match operation {
-                    Operation::Quit => {
-                        keep_running = false;
-                        break;
-                    }
-                    Operation::Reset => self.mode = Mode::View,
-                    Operation::Confirm(..) => self.mode = Mode::Confirm,
-                    Operation::UpdateConfig(_) => {}
+            for operation in self.interpreter.translate(input) {
+                if let Operation::Quit = operation {
+                    keep_running = false;
+                    break;
                 }
 
                 if let Some(change) = self.sheet.operate(operation)? {
@@ -181,11 +148,6 @@ impl Paper {
 
         Ok(keep_running)
     }
-
-    /// Converts `input` to the appropriate [`Vec`] of [`Operation`]s.
-    fn translate(&self, input: Input) -> Result<Vec<Operation>, Failure> {
-        Ok(self.interpreters.get(self.mode)?.decode(input))
-    }
 }
 
 /// An event that causes the application to stop running.
@@ -193,56 +155,17 @@ impl Paper {
 pub enum Failure {
     /// A failure in the user interface.
     #[error("user interface: {0}")]
-    Ui(#[from] ui::Error),
-    /// A mode is not stored in [`InterpreterMap`].
-    #[error("mode `{0}` is unknown")]
-    UnknownMode(Mode),
+    Ui(#[from] ui::Fault),
+    /// A failure in the translator.
+    #[error("translator: {0}")]
+    Translator(#[from] translate::Fault),
     /// A failure in the language server protocol client.
     #[error("language server protocol: {0}")]
     Lsp(#[from] LspError),
-    /// A failure in the logger.
-    #[error("logger: {0}")]
-    Log(#[from] LogError),
-}
-
-/// A failure in the logger.
-#[derive(Debug, Error)]
-pub enum LogError {
     /// A failure to create the log file.
     #[error("failed to create log file `{0}`: {1}")]
-    CreateLogFile(String, io::Error),
+    CreateLogFile(String, #[source] io::Error),
     /// A failure to initialize the logger.
     #[error("failed to initialize logger: {0}")]
-    Init(#[from] SetLoggerError),
-}
-
-/// Maps [`Mode`]s to their respective [`Interpreter`].
-#[derive(Debug)]
-struct InterpreterMap {
-    /// Map of [`Interpreter`]s.
-    map: HashMap<Mode, &'static dyn Interpreter>,
-}
-
-impl InterpreterMap {
-    /// Returns the [`Interpreter`] associated with `mode`.
-    ///
-    /// Returns [`Err`]([`Failure`]) if `mode` is not in `self.map`.
-    fn get(&self, mode: Mode) -> Result<&&dyn Interpreter, Failure> {
-        self.map.get(&mode).ok_or(Failure::UnknownMode(mode))
-    }
-}
-
-impl Default for InterpreterMap {
-    fn default() -> Self {
-        /// The [`Interpreter`] for [`Mode::View`].
-        static VIEW_INTERPRETER: ViewInterpreter = ViewInterpreter::new();
-        /// The [`Interpreter`] for [`Mode::Confirm`].
-        static CONFIRM_INTERPRETER: ConfirmInterpreter = ConfirmInterpreter::new();
-
-        let mut map: HashMap<Mode, &'static dyn Interpreter> = HashMap::new();
-
-        let _ = map.insert(Mode::View, &VIEW_INTERPRETER);
-        let _ = map.insert(Mode::Confirm, &CONFIRM_INTERPRETER);
-        Self { map }
-    }
+    InitLogger(#[from] SetLoggerError),
 }
--- src/translate.rs
+++ src/translate.rs
@@ -1,101 +1,149 @@
-//! Implements the functionality of converting [`Input`] to [`Operation`]s.
-use std::fmt::Debug;
+//! Implements the functionality of converting an [`Input`] to [`Operation`]s.
 use {
     crate::{
         app::{ConfirmAction, Operation},
-        ui::Input,
+        ui::{Input, Key},
     },
-    crossterm::event::{Event, KeyCode},
+    core::fmt::Debug,
+    enum_map::{enum_map, Enum, EnumMap},
+    lsp_types::{MessageType, ShowMessageParams},
+    parse_display::Display as ParseDisplay,
+    std::rc::Rc,
+    thiserror::Error,
 };
 
-/// Defines the functionality to convert [`Input`] to [`Operation`]s.
-pub(crate) trait Interpreter: Debug {
-    /// Converts `input` to [`Operation`]s.
-    fn decode(&self, input: Input) -> Vec<Operation>;
+/// Maps [`Mode`]s to their respective [`ModeInterpreter`].
+#[derive(Debug)]
+pub(crate) struct Interpreter {
+    /// Current [`Mode`] of the `Interpreter`.
+    mode: Mode,
+    /// Map of [`ModeInterpreter`]s.
+    map: EnumMap<Mode, ConcreteInterpreter>,
 }
 
-/// The [`Interpreter`] for [`Mode::View`].
+impl Interpreter {
+    /// Returns the [`Operation`]s that map to `input` given the current [`Mode`].
+    pub(crate) fn translate(&mut self, input: Input) -> Vec<Operation> {
+        #[allow(clippy::indexing_slicing)] // EnumMap guarantees indexing will not panic.
+        let (operations, new_mode) = self.map[self.mode].decode(input);
+
+        if let Some(mode) = new_mode {
+            self.mode = mode;
+        }
+
+        operations
+    }
+}
+
+impl Default for Interpreter {
+    fn default() -> Self {
+        Self {
+            map: enum_map! {
+                Mode::View => ConcreteInterpreter(Rc::new(ViewInterpreter::new())),
+                Mode::Confirm => ConcreteInterpreter(Rc::new(ConfirmInterpreter::new())),
+            },
+            mode: Mode::default(),
+        }
+    }
+}
+
+/// Signifies the mode of the application.
+#[derive(Copy, Clone, Debug, Enum, Eq, ParseDisplay, PartialEq, Hash)]
+#[display(style = "CamelCase")]
+pub(crate) enum Mode {
+    /// Displays the current file.
+    View,
+    /// Confirms the user's action
+    Confirm,
+}
+
+impl Default for Mode {
+    #[inline]
+    fn default() -> Self {
+        Self::View
+    }
+}
+
+/// A concrete representation of a [`ModeInterpreter`].
+#[derive(Debug)]
+struct ConcreteInterpreter(Rc<dyn ModeInterpreter>);
+
+impl ConcreteInterpreter {
+    /// Translates `input` into [`Operation`]s also returning the new [`Mode`].
+    fn decode(&self, input: Input) -> (Vec<Operation>, Option<Mode>) {
+        self.0.decode(input)
+    }
+}
+
+/// A mode is not stored in [`Interpreter`].
+#[derive(Clone, Copy, Debug, Error)]
+#[error("mode `{0}` is unknown")]
+pub struct Fault(Mode);
+
+/// Defines the functionality to convert [`Input`] to [`Operation`]s.
+pub(crate) trait ModeInterpreter: Debug {
+    /// Converts `input` to [`Operation`]s.
+    fn decode(&self, input: Input) -> (Vec<Operation>, Option<Mode>);
+}
+
+/// The [`ModeInterpreter`] for [`Mode::View`].
 #[derive(Clone, Debug)]
 pub(crate) struct ViewInterpreter {}
 
 impl ViewInterpreter {
     /// Creates a `ViewInterpreter`.
-    pub(crate) const fn new() -> Self {
+    const fn new() -> Self {
         Self {}
     }
 }
 
-impl Interpreter for ViewInterpreter {
-    fn decode(&self, input: Input) -> Vec<Operation> {
+impl ModeInterpreter for ViewInterpreter {
+    fn decode(&self, input: Input) -> (Vec<Operation>, Option<Mode>) {
         match input {
-            Input::Config(config) => vec![Operation::UpdateConfig(config)],
-            Input::User(event) => match event {
-                Event::Key(key) => match key.code {
-                    KeyCode::Esc => vec![Operation::Reset],
-                    KeyCode::Char('q') => vec![Operation::Confirm(ConfirmAction::Quit)],
-                    KeyCode::Enter
-                    | KeyCode::Backspace
-                    | KeyCode::Left
-                    | KeyCode::Right
-                    | KeyCode::Up
-                    | KeyCode::Down
-                    | KeyCode::Home
-                    | KeyCode::End
-                    | KeyCode::PageUp
-                    | KeyCode::PageDown
-                    | KeyCode::Tab
-                    | KeyCode::BackTab
-                    | KeyCode::Delete
-                    | KeyCode::Insert
-                    | KeyCode::F(..)
-                    | KeyCode::Char(..)
-                    | KeyCode::Null => vec![],
-                },
-                Event::Mouse(..) | Event::Resize(..) => vec![],
-            },
+            Input::Setting(config) => (vec![Operation::UpdateConfig(config)], None),
+            Input::Key { key: Key::Esc, .. } => (vec![Operation::Reset], None),
+            Input::Key {
+                key: Key::Char('q'),
+                ..
+            } => (
+                vec![Operation::Confirm(ConfirmAction::Quit)],
+                Some(Mode::Confirm),
+            ),
+            Input::Glitch(fault) => (
+                vec![Operation::Alert(ShowMessageParams {
+                    typ: MessageType::Error,
+                    message: format!("{}", fault),
+                })],
+                None,
+            ),
+            Input::Key { .. } | Input::Mouse | Input::Resize { .. } => (vec![], None),
         }
     }
 }
 
-/// The [`Interpreter`] for [`Mode::Confirm`].
+/// The [`ModeInterpreter`] for [`Mode::Confirm`].
 #[derive(Clone, Debug)]
 pub(crate) struct ConfirmInterpreter {}
 
 impl ConfirmInterpreter {
     /// Creates a new `ConfirmInterpreter`.
-    pub(crate) const fn new() -> Self {
+    const fn new() -> Self {
         Self {}
     }
 }
 
-impl Interpreter for ConfirmInterpreter {
-    fn decode(&self, input: Input) -> Vec<Operation> {
+impl ModeInterpreter for ConfirmInterpreter {
+    fn decode(&self, input: Input) -> (Vec<Operation>, Option<Mode>) {
         match input {
-            Input::User(event) => match event {
-                Event::Key(key) => match key.code {
-                    KeyCode::Char('y') => vec![Operation::Quit],
-                    KeyCode::Char(..)
-                    | KeyCode::Backspace
-                    | KeyCode::Enter
-                    | KeyCode::Left
-                    | KeyCode::Right
-                    | KeyCode::Up
-                    | KeyCode::Down
-                    | KeyCode::Home
-                    | KeyCode::End
-                    | KeyCode::PageUp
-                    | KeyCode::PageDown
-                    | KeyCode::Tab
-                    | KeyCode::BackTab
-                    | KeyCode::Delete
-                    | KeyCode::Insert
-                    | KeyCode::F(..)
-                    | KeyCode::Null
-                    | KeyCode::Esc => vec![Operation::Reset],
-                },
-                Event::Mouse(..) | Event::Resize(..) => vec![],
-            },
-            Input::Config(..) => vec![],
+            Input::Key {
+                key: Key::Char('y'),
+                ..
+            } => (vec![Operation::Quit], None),
+            Input::Key { .. }
+            | Input::Mouse
+            | Input::Resize { .. }
+            | Input::Setting(..)
+            | Input::Glitch(..) => (vec![Operation::Reset], Some(Mode::View)),
         }
     }
 }
@@ -104,7 +152,7 @@ impl Interpreter for ConfirmInterpreter {
 #[cfg(test)]
 mod test_view {
     use super::*;
-    use crossterm::event::{KeyEvent, KeyModifiers};
+    use crate::ui::Modifiers;
 
     /// The `q` key shall confirm the user wants to quit.
     #[test]
@@ -112,20 +160,23 @@ mod test_view {
         let view = ViewInterpreter::new();
 
         assert_eq!(
-            view.decode(Input::User(Event::Key(KeyEvent::new(
-                KeyCode::Char('q'),
-                KeyModifiers::empty()
-            )))),
-            vec![Operation::Confirm(ConfirmAction::Quit)]
+            view.decode(Input::Key {
+                key: Key::Char('q'),
+                modifiers: Modifiers::empty()
+            }),
+            (
+                vec![Operation::Confirm(ConfirmAction::Quit)],
+                Some(Mode::Confirm)
+            )
         );
     }
 }
 
-/// Tests decoding user input while in the View mode.
+/// Tests decoding user input while in the Confirm mode.
 #[cfg(test)]
 mod test_confirm {
     use super::*;
-    use crossterm::event::{KeyEvent, KeyModifiers};
+    use crate::ui::Modifiers;
 
     static INTERPRETER: ConfirmInterpreter = ConfirmInterpreter::new();
 
@@ -133,37 +184,37 @@ mod test_confirm {
     #[test]
     fn confirm() {
         assert_eq!(
-            INTERPRETER.decode(Input::User(Event::Key(KeyEvent::new(
-                KeyCode::Char('y'),
-                KeyModifiers::empty()
-            )))),
-            vec![Operation::Quit]
+            INTERPRETER.decode(Input::Key {
+                key: Key::Char('y'),
+                modifiers: Modifiers::empty()
+            }),
+            (vec![Operation::Quit], None)
         );
     }
 
-    /// Any other key shall cancel the action.
+    /// Any other key shall cancel the action, resetting the application to View mode.
     #[test]
     fn cancel() {
         assert_eq!(
-            INTERPRETER.decode(Input::User(Event::Key(KeyEvent::new(
-                KeyCode::Char('n'),
-                KeyModifiers::empty()
-            )))),
-            vec![Operation::Reset]
+            INTERPRETER.decode(Input::Key {
+                key: Key::Char('n'),
+                modifiers: Modifiers::empty()
+            }),
+            (vec![Operation::Reset], Some(Mode::View))
         );
         assert_eq!(
-            INTERPRETER.decode(Input::User(Event::Key(KeyEvent::new(
-                KeyCode::Char('c'),
-                KeyModifiers::empty()
-            )))),
-            vec![Operation::Reset]
+            INTERPRETER.decode(Input::Key {
+                key: Key::Char('c'),
+                modifiers: Modifiers::empty()
+            }),
+            (vec![Operation::Reset], Some(Mode::View))
         );
         assert_eq!(
-            INTERPRETER.decode(Input::User(Event::Key(KeyEvent::new(
-                KeyCode::Char('1'),
-                KeyModifiers::empty()
-            )))),
-            vec![Operation::Reset]
+            INTERPRETER.decode(Input::Key {
+                key: Key::Char('1'),
+                modifiers: Modifiers::empty()
+            }),
+            (vec![Operation::Reset], Some(Mode::View))
         );
     }
 }
